5f95669fd171751ff3c213d2c5f8fdcf6189d2e5
Shafay Abid <shafayabid4@gmail.com>
2024-03-26 21:54:40 +0500

    functional digit buttons

---
+++ script.js
@@ -0,0 +1,54 @@
+
+const btn0 = document.querySelector(".btn-0");
+const btn1 = document.querySelector(".btn-1");
+const btn2 = document.querySelector(".btn-2");
+const btn3 = document.querySelector(".btn-3");
+const btn4 = document.querySelector(".btn-4");
+const btn5 = document.querySelector(".btn-5");
+const btn6 = document.querySelector(".btn-6");
+const btn7 = document.querySelector(".btn-7");
+const btn8 = document.querySelector(".btn-8");
+const btn9 = document.querySelector(".btn-9");
+
+const screen = document.querySelector(".screen");
+
+btn0.addEventListener("click", function (e) {
+    screen.innerText = screen.innerText + e.target.innerText;
+  });
+
+btn1.addEventListener("click", function (e) {
+    screen.innerText = screen.innerText + e.target.innerText;
+  });
+
+btn2.addEventListener("mouseover", function (e) {
+    screen.innerText = screen.innerText + e.target.innerText;
+  });
+
+btn3.addEventListener("mouseover", function (e) {
+    screen.innerText = screen.innerText + e.target.innerText;
+  });
+
+btn4.addEventListener("mouseover", function (e) {
+    screen.innerText = screen.innerText + e.target.innerText;
+  });
+
+btn5.addEventListener("mouseover", function (e) {
+    screen.innerText = screen.innerText + e.target.innerText;
+  });
+
+btn6.addEventListener("mouseover", function (e) {
+    screen.innerText = screen.innerText + e.target.innerText;
+  });
+
+btn7.addEventListener("mouseover", function (e) {
+    screen.innerText = screen.innerText + e.target.innerText;
+  });
+
+btn8.addEventListener("mouseover", function (e) {
+    screen.innerText = screen.innerText + e.target.innerText;
+  });
+
+btn9.addEventListener("mouseover", function (e) {
+    screen.innerText = screen.innerText + e.target.innerText;
+  });
+
