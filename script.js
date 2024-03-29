f3d8f171ce9146213ad80407af32bfb255587ff4
Shafay Abid <shafayabid4@gmail.com>
2024-03-30 00:14:47 +0500

    basic functionality complete

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let firstNumber = null;
 let secondNumber = null;
+let singleNumber = null;
 let result = null;
+let lastEnteredOperator = null;
 
 
 const btn0 = document.querySelector(".btn-0");
@@ -20,66 +22,199 @@ const btnSub = document.querySelector(".btn-sub");
 const btnAdd = document.querySelector(".btn-add");
 const btnEquals = document.querySelector(".btn-equals");
 
+const btnclr = document.querySelector(".btn-clr");
 const screen = document.querySelector(".screen");
 
 btn0.addEventListener("click", function (e) {
+  if (screen.innerText != "+" && screen.innerText != "-" && screen.innerText != "/" && screen.innerText != "*") {
     screen.innerText = screen.innerText + e.target.innerText;
-  });
+  } else {
+    screen.innerText = e.target.innerText
+  }
+});
 
 btn1.addEventListener("click", function (e) {
+  if (screen.innerText != "+" && screen.innerText != "-" && screen.innerText != "/" && screen.innerText != "*") {
     screen.innerText = screen.innerText + e.target.innerText;
-  });
+  } else {
+    screen.innerText = e.target.innerText
+  }
+});
 
 btn2.addEventListener("click", function (e) {
+  if (screen.innerText != "+" && screen.innerText != "-" && screen.innerText != "/" && screen.innerText != "*") {
     screen.innerText = screen.innerText + e.target.innerText;
-  });
+  } else {
+    screen.innerText = e.target.innerText
+  }
+});
 
 btn3.addEventListener("click", function (e) {
+  if (screen.innerText != "+" && screen.innerText != "-" && screen.innerText != "/" && screen.innerText != "*") {
     screen.innerText = screen.innerText + e.target.innerText;
-  });
+  } else {
+    screen.innerText = e.target.innerText
+  }
+});
 
 btn4.addEventListener("click", function (e) {
+  if (screen.innerText != "+" && screen.innerText != "-" && screen.innerText != "/" && screen.innerText != "*") {
     screen.innerText = screen.innerText + e.target.innerText;
-  });
+  } else {
+    screen.innerText = e.target.innerText
+  }
+});
 
 btn5.addEventListener("click", function (e) {
+  if (screen.innerText != "+" && screen.innerText != "-" && screen.innerText != "/" && screen.innerText != "*") {
     screen.innerText = screen.innerText + e.target.innerText;
-  });
+  } else {
+    screen.innerText = e.target.innerText
+  }
+});
 
 btn6.addEventListener("click", function (e) {
+  if (screen.innerText != "+" && screen.innerText != "-" && screen.innerText != "/" && screen.innerText != "*") {
     screen.innerText = screen.innerText + e.target.innerText;
-  });
+  } else {
+    screen.innerText = e.target.innerText
+  }
+});
 
 btn7.addEventListener("click", function (e) {
+  if (screen.innerText != "+" && screen.innerText != "-" && screen.innerText != "/" && screen.innerText != "*") {
     screen.innerText = screen.innerText + e.target.innerText;
-  });
+  } else {
+    screen.innerText = e.target.innerText
+  }
+});
 
 btn8.addEventListener("click", function (e) {
+  if (screen.innerText != "+" && screen.innerText != "-" && screen.innerText != "/" && screen.innerText != "*") {
     screen.innerText = screen.innerText + e.target.innerText;
-  });
+  } else {
+    screen.innerText = e.target.innerText
+  }
+});
 
 btn9.addEventListener("click", function (e) {
+  if (screen.innerText != "+" && screen.innerText != "-" && screen.innerText != "/" && screen.innerText != "*") {
     screen.innerText = screen.innerText + e.target.innerText;
-  });
+  } else {
+    screen.innerText = e.target.innerText
+  }
+});
 
 btnAdd.addEventListener("click", function (e) {
-    if(screen.innerText != ""){
-      screen.innerText = e.target.innerText;
+  if (screen.innerText != "") {
+    if (firstNumber == null) {
+      firstNumber = screen.innerText
+    } else if (secondNumber == null) {
+      secondNumber = screen.innerText
+      calculate(firstNumber, secondNumber, lastEnteredOperator)
+    } else if (firstNumber != null && secondNumber != null && result != null) {
+      singleNumber = screen.innerText
+      calculate(result, singleNumber, lastEnteredOperator)
     }
-  });
+    console.log(lastEnteredOperator)
+    lastEnteredOperator = e.target.innerText;
+    screen.innerText = e.target.innerText;
+  }
+});
 btnMul.addEventListener("click", function (e) {
-    if(screen.innerText != ""){
-      screen.innerText = e.target.innerText;
+  if (screen.innerText != "") {
+    if (firstNumber == null) {
+      firstNumber = screen.innerText
+    } else if (secondNumber == null) {
+      secondNumber = screen.innerText
+      calculate(firstNumber, secondNumber, lastEnteredOperator)
+    } else if (firstNumber != null && secondNumber != null && result != null) {
+      singleNumber = screen.innerText
+      calculate(result, singleNumber, lastEnteredOperator)
     }
-  });
+    console.log(lastEnteredOperator)
+    lastEnteredOperator = e.target.innerText;
+    screen.innerText = e.target.innerText;
+  }
+});
 btnDiv.addEventListener("click", function (e) {
-    if(screen.innerText != ""){
-      screen.innerText = e.target.innerText;
+  if (screen.innerText != "") {
+    if (firstNumber == null) {
+      firstNumber = screen.innerText
+    } else if (secondNumber == null) {
+      secondNumber = screen.innerText
+      calculate(firstNumber, secondNumber, lastEnteredOperator)
+    } else if (firstNumber != null && secondNumber != null && result != null) {
+      singleNumber = screen.innerText
+      calculate(result, singleNumber, lastEnteredOperator)
     }
-  });
+    console.log(lastEnteredOperator)
+    lastEnteredOperator = e.target.innerText;
+    screen.innerText = e.target.innerText;
+  }
+});
 btnSub.addEventListener("click", function (e) {
-    if(screen.innerText != ""){
-      screen.innerText = e.target.innerText;
+  if (screen.innerText != "") {
+    if (firstNumber == null) {
+      firstNumber = screen.innerText
+    } else if (secondNumber == null) {
+      secondNumber = screen.innerText
+      calculate(firstNumber, secondNumber, lastEnteredOperator)
+    } else if (firstNumber != null && secondNumber != null && result != null) {
+      singleNumber = screen.innerText
+      calculate(result, singleNumber, lastEnteredOperator)
     }
-  });
+    console.log(lastEnteredOperator)
+    lastEnteredOperator = e.target.innerText;
+    screen.innerText = e.target.innerText;
+  }
+});
+
+btnEquals.addEventListener("click", function (e) {
+  if (result == null) {
+    secondNumber = screen.innerText
+    calculate(firstNumber, secondNumber, lastEnteredOperator);
+    screen.innerText = result;
+  } else {
+    singleNumber = screen.innerText
+    calculate(result, singleNumber, lastEnteredOperator);
+    screen.innerText = result;
+  }
+})
+
+btnclr.addEventListener("click", function (e) {
+  firstNumber = null;
+  secondNumber = null;
+  singleNumber = null;
+  result = null;
+  lastEnteredOperator = null;
+
+  screen.innerText = ""
+})
+
+function calculate(num1, num2, operator) {
+  console.log("inside fun" + lastEnteredOperator)
+  switch (operator) {
+    case '+':
+      result = Number(num1) + Number(num2);
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    case '/':
+      if (num2 !== 0) {
+        result = num1 / num2;
+      } else {
+        result = "Cannot divide by zero";
+      }
+      break;
+    default:
+      result = "Invalid operator";
+  }
+  console.log(`${num1} ${operator} ${num2} = ${result}`)
+}
+
 
